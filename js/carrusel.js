e4248b35d183dfba0c1008f22633d3dacc323803
valentinoarga <valentinoarga01@gmail.com>
2023-12-06 23:44:46 -0300

    agrego carrusel de pagina principal

---
+++ js/carrusel.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".mySwiper", {
+
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grabCursor:true,
+    loop:true,
+    breakpoints: {
+        991: {
+            slidesPerView:4
+        }
+    }
+})
+
